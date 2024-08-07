7e360babaa515131772456ea188b24a9dcabf2d3
Alik sexy <alik.want.you@gmail.com>
2024-08-07 22:21:15 +0300

    feat: add script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  const textN = String(Math.abs(n))
+	const arrayN = textN.split('')
+	const reverseArrayN = arrayN.reverse()
+	const reverseTextN = reverseArrayN.join('')
+	return +reverseTextN
 }
